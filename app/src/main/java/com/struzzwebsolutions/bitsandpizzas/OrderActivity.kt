00b11600a2cab20a651f0b9cd914b03f10b615d8
keith <keith_struzzieri@yahoo.com>
2018-09-11 23:05:47 -0400

    Add FAB

--- app/src/main/java/com/struzzwebsolutions/bitsandpizzas/OrderActivity.kt
+++ app/src/main/java/com/struzzwebsolutions/bitsandpizzas/OrderActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.support.v7.app.ActionBar
+import android.view.View
+import android.support.design.widget.Snackbar
+import android.widget.Toast
 
 class OrderActivity : AppCompatActivity() {
 
@@ -14,5 +17,20 @@ class OrderActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun onClickDone(view: View) {
+        val text: CharSequence = "Your order has been updated"
+        val duration: Int = Snackbar.LENGTH_SHORT
+        val snackbar: Snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration)
+                .setAction("Undo", View.OnClickListener {
+                    val toast = Toast.makeText(
+                            this,
+                            "Undone!",
+                            Toast.LENGTH_LONG)
+                            .show()
+                })
+        snackbar.show()
+
     }
 }
